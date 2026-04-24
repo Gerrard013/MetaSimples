2a13eb8e3562861b55bd7cfb0e3e4fceedaf89f8
Gerrard013 <seu-email@aquidogithub.com>
2026-04-24 11:46:38 -0300

    fix: troca pg8000 por psycopg3

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,10 @@ def _fix_database_url(database_url: str) -> str:
     database_url = database_url.strip()
 
     if database_url.startswith("postgresql://"):
-        return database_url.replace("postgresql://", "postgresql+pg8000://", 1)
+        return database_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
     if database_url.startswith("postgres://"):
-        return database_url.replace("postgres://", "postgresql+pg8000://", 1)
+        return database_url.replace("postgres://", "postgresql+psycopg://", 1)
 
     return database_url
 
